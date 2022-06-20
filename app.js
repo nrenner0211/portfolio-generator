a72aaadfbade3c6e4d4fc7e8b86a4371a2ea5fa8
nrenner <nrenner0211@gmail.com>
2022-06-20 01:23:44 -0400

    page-template

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// const printProfileData = (profileDataArr) => {
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
   
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
