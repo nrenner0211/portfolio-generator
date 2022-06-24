0a1e4b1adf637db4a78d0d0e3b944acc14e9dabb
nrenner <nrenner0211@gmail.com>
2022-06-24 01:22:54 -0400

    bugs

--- src/page-template.js
+++ src/page-template.js
@@ -25,7 +25,7 @@ const generateProjects = projectsArr => {
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(', ')}
+              ${languages.map(language => language).join(',')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
